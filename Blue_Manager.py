3040617dd07e78b99f7658a00af837b8638c82d7
Dan Shimada <danshimada@gmail.com>
2021-01-18 00:16:17 -0300

    Novas opcoes funcionando

--- Blue_Manager.py
+++ Blue_Manager.py
@@ -1,115 +1,264 @@
 import pandas as pd
-
-#lista_cabecalho = ['id', 'Nome', 'Nome_completo', 'Endereco', 'Numero', 'Cidade', 'Estado', 'Data_Nascimento', 'Nome_Responsavel']
-lista_id_aluno = []
-lista_nome = []
-lista_nome_completo = []
-lista_rua = []
-lista_numero = []
-lista_cidade = []
-lista_estado = []
-lista_data_Nascimento = []
-lista_nome_Responsavel = []
-
-def appendDFToCSV_void(df, csvFilePath, sep=";"):
-    import os
-    if not os.path.isfile('alunos_teste_2.csv'):
-        df.to_csv('alunos_teste_2.csv', mode='a', index=False, sep=sep, header=True)
-    elif len(df.columns) != len(pd.read_csv('alunos_teste_2.csv', nrows=1, sep=sep).columns):
-        raise Exception("Columns do not match!! Dataframe has " + str(
-            len(df.columns)) + " columns. CSV file has " + str(
-            len(pd.read_csv('alunos_teste_2.csv', nrows=1, sep=sep).columns)) + " columns.")
-    elif not (df.columns == pd.read_csv('alunos_teste_2.csv', nrows=1, sep=sep).columns).all():
-        raise Exception("Columns and column order of dataframe and csv file do not match!!")
-    else:
-        df.to_csv('alunos_teste_2.csv', mode='a', index=False, sep=sep, header=False)
+import numpy as np
 
 def main():
     cond = True
     while cond:
 
-        print("#####################################")
-        print("#      BLUE STUDENT MANAGER         #")
-        print("#####################################")
-        print("| 1 - Cadastrar aluno               |")
-        print("| 2 - Listar alunos                 |")
-        print("| 3 - Excluir aluno                 |")
-        print("| 4 - Cadstrar matricula            |")
-        print("| 5 - Alterar notas                 |")
-        print("| 6 - Alterar situacao do pagamento |")
-        print("| 7 - Sair                          |")
-        print("|___________________________________|")
-        opc = int(input("Escolha uma opcao: "))
-
+        print(' ----------------------------------- ')
+        print('|      BLUE STUDENT MANAGER         |')
+        print(' -----------------------------------')
+        print('| 1 - Criar planilhas               |') #OK
+        print('| 2 - Cadastrar aluno               |') #OK
+        print('| 3 - Cadastrar matricula           |') #OK
+        print('| 4 - Listar alunos                 |') #OK
+        print('| 5 - Excluir aluno                 |')
+        print('| 6 - Alterar notas                 |')
+        print('| 7 - Alterar situacao do pagamento |')
+        print('| 8 - Sair                          |') #OK
+        print('|___________________________________|')
+        opc = int(input('Escolha uma opcao: '))
+# CRIAR ARQUIVOS
         if opc == 1:
-            print('Para voltar no meu anterior, digite -1')
+            print(' ----------------------------------- ')
+            print('|          CRIAR PLANILHAS          |')
+            print(' ----------------------------------- ')
+            print('| 1 - Criar planilha Alunos         |')
+            print('| 2 - Criar planilha Matricula      |')
+            opc_plan = int(input('Escolha uma opcao: '))
+# CADASTRAR CABECALHO DO ALUNO
+            if opc_plan == 1:
+                lista_id_aluno = []
+                lista_nome = []
+                lista_nome_completo = []
+                lista_endereco = []
+                lista_numero = []
+                lista_cidade = []
+                lista_estado = []
+                lista_data_Nascimento = []
+                lista_nome_Responsavel = []
 
-            id_aluno = int(input("ID: "))
+                cabecalho_Alunos = {
+                    'id': lista_id_aluno,
+                    'Nome': lista_nome,
+                    'Nome_completo': lista_nome_completo,
+                    'Endereco': lista_endereco,
+                    'Numero': lista_numero,
+                    'Cidade': lista_cidade,
+                    'Estado': lista_estado,
+                    'Data_Nascimento': lista_data_Nascimento,
+                    'Nome_Responsavel': lista_nome_Responsavel
+                }
+                df_cabecalho_Alunos = pd.DataFrame(cabecalho_Alunos)
+                df_cabecalho_Alunos.to_csv('Alunos.csv', header=True, index=False, sep=';')
+                print('')
+                print('Planilha Alunos criada')
+                print('')
+                print(' =================================== ')
 
-            if id_aluno == -1:
-                print('Cadastro finalizado.')
-                break
-            lista_id_aluno.append(id_aluno)
+# CADASTRAR CABECALHO DA MATRICULA
+            if opc_plan == 2:
+                lista_Cod = []
+                lista_Cursos = []
+                lista_Horario = []
+                lista_id_Aluno = []
+                lista_Pagamento = []
+                lista_P1 = []
+                lista_P2 = []
+                lista_P3 = []
+                lista_P4 = []
+                lista_Media = []
+                lista_Situacao = []
 
-            primeiro_nome = input("Primeiro Nome: ")
-            lista_nome.append(primeiro_nome)
+                cabecalho_Curso_Alunos = {
+                    'Cod': lista_Cod,
+                    'Cursos': lista_Cursos,
+                    'Horario': lista_Horario,
+                    'id_Aluno': lista_id_Aluno,
+                    'Pagamento': lista_Pagamento,
+                    'P1': lista_P1,
+                    'P2': lista_P2,
+                    'P3': lista_P3,
+                    'P4': lista_P4,
+                    'Media': lista_Media,
+                    'Situacao': lista_Situacao
+                }
+                df_cabecalho_Cursos_Alunos = pd.DataFrame(cabecalho_Curso_Alunos)
+                df_cabecalho_Cursos_Alunos.to_csv('Matricula.csv', header=True, index=False, sep=';')
+                print('')
+                print('Planilha Matricula criada')
+                print('')
+                print(' =================================== ')
 
-            nomeCompleto = input("Nome Completo: ")
-            lista_nome_completo.append(nomeCompleto)
+# CADASTRAR ALUNO
+        if opc == 2:
+            lista_id_aluno_aux = []
+            lista_nome_aux = []
+            lista_nome_completo_aux = []
+            lista_endereco_aux = []
+            lista_numero_aux = []
+            lista_cidade_aux= []
+            lista_estado_aux = []
+            lista_data_Nascimento_aux = []
+            lista_nome_Responsavel_aux = []
 
-            rua = input("Endereco: ")
-            lista_rua.append(rua)
+            id_aluno = int(input('ID: '))
+            lista_id_aluno_aux.append(id_aluno)
 
-            numero = int(input("Numero: "))
-            lista_numero.append(numero)
+            primeiro_nome = input('Primeiro Nome: ')
+            lista_nome_aux.append(primeiro_nome)
 
-            cidade = input("Cidade: ")
-            lista_cidade.append(cidade)
+            nomeCompleto = input('Nome Completo: ')
+            lista_nome_completo_aux.append(nomeCompleto)
 
-            estado = input("Estado: ")
-            lista_estado.append(estado)
+            rua = input('Endereco: ')
+            lista_endereco_aux.append(rua)
 
-            data_Nascimento = input("Data de Nascimento: ")
-            lista_data_Nascimento.append(data_Nascimento)
+            numero = int(input('Numero: '))
+            lista_numero_aux.append(numero)
 
-            nome_Responsavel = input("Nome do responsavel: ")
-            lista_nome_Responsavel.append(nome_Responsavel)
+            cidade = input('Cidade: ')
+            lista_cidade_aux.append(cidade)
 
-            dados_alunos = list(zip(lista_id_aluno, lista_nome, lista_nome_completo, lista_rua, lista_numero, lista_cidade, lista_estado, lista_data_Nascimento, lista_nome_Responsavel))
+            estado = input('Estado: ')
+            lista_estado_aux.append(estado)
 
-            df = pd.DataFrame(dados_alunos, columns=
-            [
-                'id',
-                'Nome',
-                'Nome_completo',
-                'Endereco',
-                'Numero',
-                'Cidade',
-                'Estado',
-                'Data_Nascimento',
-                'Nome_Responsavel'
-            ])
-            appendDFToCSV_void(df, 'alunos_teste_2.csv', sep=';')
+            data_Nascimento = input('Data de Nascimento: ')
+            lista_data_Nascimento_aux.append(data_Nascimento)
 
-            #df.to_csv('alunos_teste_2.csv', index=False, sep=';', header=True)
+            nome_Responsavel = input('Nome do responsavel: ')
+            lista_nome_Responsavel_aux.append(nome_Responsavel)
 
-        if opc == 2: # OK
-            with open('alunos_teste_2.csv', mode='r') as ReadFile:
-                df = pd.read_csv(ReadFile, sep=None)
+            #dados_alunos = list(zip(lista_id_aluno, lista_nome, lista_nome_completo, lista_rua, lista_numero, lista_cidade, lista_estado, lista_data_Nascimento, lista_nome_Responsavel))
+            aluno = {
+                'id': lista_id_aluno_aux,
+                'Nome': lista_nome_aux,
+                'Nome_completo': lista_nome_completo_aux,
+                'Endereco': lista_endereco_aux,
+                'Numero': lista_numero_aux,
+                'Cidade': lista_cidade_aux,
+                'Estado': lista_estado_aux,
+                'Data_Nascimento': lista_data_Nascimento_aux,
+                'Nome_Responsavel': lista_nome_Responsavel_aux
+            }
+            df_aluno = pd.DataFrame(aluno)
+            # appendDFToCSV_void(df, 'alunos_teste_2.csv', sep=';')
+            df_aluno.to_csv('Alunos.csv', header=False, index=False, sep=';', mode='a')
+            print('')
+            print('Aluno {}, de ID={}, foi cadastrado.'.format(nomeCompleto, id_aluno))
+            print('')
+            print(' =================================== ')
+
+# CADASTRAR MATRICULA
+        if opc == 3:
+            lista_Cod_aux = []
+            lista_Cursos_aux = []
+            lista_Horario_aux = []
+            lista_id_Aluno_aux = []
+            lista_Pagamento_aux = []
+            lista_P1_aux = []
+            lista_P2_aux = []
+            lista_P3_aux = []
+            lista_P4_aux = []
+            lista_Media_aux = []
+            lista_Situacao_aux = []
+
+            print(' =================================== ')
+            print('Codigo dos cursos:')
+            print(' Basico: curso01')
+            print(' Intermediario: curso02')
+            print(' Avancado: curso03')
+            print('')
+
+            cod_mat = input('Codigo da matricula: ')
+            lista_Cod_aux.append(cod_mat)
+
+            if cod_mat == 'curso01':
+                curso_mat = 'Basico'
+            elif cod_mat == 'curso02':
+                curso_mat = 'Intermediario'
+            elif cod_mat == 'curso03':
+                curso_mat = 'Avancado'
+            lista_Cursos_aux.append(curso_mat)
+            print('Curso: {}'.format(curso_mat))
+
+            horario_mat = input('Periodo: ')
+            lista_Horario_aux.append(horario_mat)
+
+            id_aluno_mat = int(input('ID aluno: '))
+            lista_id_Aluno_aux.append(id_aluno_mat)
+
+            pagamento_mat = input('Pagamento/Em Debito: ')
+            lista_Pagamento_aux.append(pagamento_mat)
+
+            p1_mat = float(input('P1: '))
+            lista_P1_aux.append(p1_mat)
+
+            p2_mat = float(input('P2: '))
+            lista_P2_aux.append(p2_mat)
+
+            p3_mat = float(input('P3: '))
+            lista_P3_aux.append(p3_mat)
+
+            p4_mat = float(input('P4: '))
+            lista_P4_aux.append(p4_mat)
+
+            if p1_mat == 0 & p2_mat == 0 & p3_mat & p4_mat == 0:
+                media_mat = 0
+                situacao_mat = 'Reprovado'
+            else:
+                media_mat = (p1_mat + p2_mat + p3_mat + p4_mat) / 4
+
+            lista_Media_aux.append(media_mat)
+            print('Media: {}'.format(media_mat))
+
+            if media_mat >= 7:
+                situacao_mat = 'Aprovado'
+            elif media_mat < 7:
+                situacao_mat = 'Reprovado'
+
+            lista_Situacao_aux.append(situacao_mat)
+            print('Situacao: {}'.format(situacao_mat))
+
+            Matricula = {
+                'Cod': lista_Cod_aux,
+                'Cursos': lista_Cursos_aux,
+                'Horario': lista_Horario_aux,
+                'id_Aluno': lista_id_Aluno_aux,
+                'Pagamento': lista_Pagamento_aux,
+                'P1': lista_P1_aux,
+                'P2': lista_P2_aux,
+                'P3': lista_P3_aux,
+                'P4': lista_P4_aux,
+                'Media': lista_Media_aux,
+                'Situacao': lista_Situacao_aux
+            }
+            df_cabecalho_Cursos_Alunos = pd.DataFrame(Matricula)
+            df_cabecalho_Cursos_Alunos.to_csv('Matricula.csv', header=False, index=False, sep=';', mode='a')
+            print('')
+            print('Matricula do aluno de ID={} foi cadastrado'.format(id_aluno_mat))
+            print('')
+            print(' =================================== ')
+
+# LISTAR ALUNOS
+        if opc == 4:
+            with open('Alunos.csv', mode='r') as ReadFile:
+                df = pd.read_csv(ReadFile, sep=None, engine='python')
                 print(df)
+            print('')
 
-            #Location = r'C:\Users\Dan\Desktop\Blue_Cadastro_2\alunos_teste_2.csv'
-            #df = pd.read_csv(Location)
-            #print(df)
-
-        if opc == 3: # OK
+# DELETAR ALUNO
+        if opc == 5:
             deletar_aluno_index = int(input('Escreva o index do aluno a ser deletado: '))
             df.drop(deletar_aluno_index, inplace=True)
             a = pd.DataFrame(df)
-            a.to_csv('alunos_teste_2.csv', sep=';')
+            a.to_csv('Alunos.csv', sep=';')
             #df.to_csv('alunos_teste_3.csv', index=False)
-            print("Aluno deletado")
-        if opc == 7:
+            print('Aluno deletado')
+            print('')
+
+        if opc == 8:
             break
 
 main()
+
